37cc53bd7b39e4e83ab85f348040970a0a68ab57
MoncefBou <moncefboughal@hotmail.fr>
2022-06-18 11:48:10 +0200

    first test -RED

--- test/tennis.test.ts
+++ test/tennis.test.ts
@@ -3,6 +3,6 @@ const { scoreToDisplay } = require('../src/tennis')
 describe("Tennis", () => {
     test('Should return Love-Love if no one scored', () => {
         const result = scoreToDisplay()
-        expect(result).toBe('15-15')
+        expect(result).toBe('Love-Love')
     })
 })
