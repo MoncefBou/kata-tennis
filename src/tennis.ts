ad2910d84989a1587dafc4c4602db17d8d1b6869
MoncefBou <moncefboughal@hotmail.fr>
2022-06-19 16:33:33 +0200

    All scores without winner -Green

--- src/tennis.ts
+++ src/tennis.ts
@@ -1,10 +1,51 @@
-export const scoreToDisplay = (scorePlayerA: Number, scorePlayerB: Number) => {
-    if (scorePlayerA === 0 && scorePlayerB === 0) {
-        return 'Love-Love'
-    } 
-
-    if (scorePlayerA === 1 && scorePlayerB === 0) {
-        return '15-Love'
+export const scoreToDisplay = (scorePlayerA: Number, scorePlayerB: Number): String => {
+    if (scorePlayerA === 3 && scorePlayerB === 3) {
+        return 'Deuce'
     }
+
+    let scoreToDisplayPlayerA: String;
+    let scoreToDisplayPlayerB: String;
+
+    switch (scorePlayerA) {
+        case 0:
+            scoreToDisplayPlayerA = "Love"
+            break;
+        case 1:
+            scoreToDisplayPlayerA = "15"
+            break;
+        case 2:
+            scoreToDisplayPlayerA = "30"
+            break;
+        case 3:
+            scoreToDisplayPlayerA = "40"
+            break;
+        case 4:
+            scoreToDisplayPlayerA = "Advantage"
+            break;
+        default:
+            return 'Error with the score of player A'
+    }
+
+    switch (scorePlayerB) {
+        case 0:
+            scoreToDisplayPlayerB = "Love"
+            break;
+        case 1:
+            scoreToDisplayPlayerB = "15"
+            break;
+        case 2:
+            scoreToDisplayPlayerB = "30"
+            break;
+        case 3:
+            scoreToDisplayPlayerB = "40"
+            break;
+        case 4:
+            scoreToDisplayPlayerB = "Advantage"
+            break;
+        default:
+            return 'Error with the score of player B'
+    }
+
+    return `${scoreToDisplayPlayerA}-${scoreToDisplayPlayerB}`
 }
 
--- test/tennis.test.ts
+++ test/tennis.test.ts
@@ -1,12 +1,70 @@
 const { scoreToDisplay } = require('../src/tennis')
 
 describe("Tennis", () => {
-    test('Should display Love-Love if no one scores', () => {
-        const result = scoreToDisplay(0,0)
-        expect(result).toBe('Love-Love')
-    })
-    test('Should display 15-Love if Player A scores a point', () => {
-        const result = scoreToDisplay(1,0)
-        expect(result).toBe('15-Love')
+    describe('All scores possible without winner', () => {
+        test('Should display Love-Love if no one scores', () => {
+            const result = scoreToDisplay(0, 0)
+            expect(result).toBe('Love-Love')
+        })
+        test('Should display 15-Love if Player A scores 1 point', () => {
+            const result = scoreToDisplay(1, 0)
+            expect(result).toBe('15-Love')
+        })
+        test('Should display 30-Love if Player A scores 2 points', () => {
+            const result = scoreToDisplay(2, 0)
+            expect(result).toBe('30-Love')
+        })
+        test('Should display 40-Love if Player A scores 3 points', () => {
+            const result = scoreToDisplay(3, 0)
+            expect(result).toBe('40-Love')
+        })
+        test('Should display 15-15 if they score 1 point each', () => {
+            const result = scoreToDisplay(1, 1)
+            expect(result).toBe('15-15')
+        })
+        test('Should display 30-15 if Player A scores 2 points and Player B scores 1 point', () => {
+            const result = scoreToDisplay(2, 1)
+            expect(result).toBe('30-15')
+        })
+        test('Should display 30-30 if Player A scores 2 points and Player B scores 2 points', () => {
+            const result = scoreToDisplay(2, 2)
+            expect(result).toBe('30-30')
+        })
+        test('Should display 40-30 if Player A scores 3 points and Player B scores 2 points', () => {
+            const result = scoreToDisplay(3, 2)
+            expect(result).toBe('40-30')
+        })
+        test('Should display Deuce if Player A scores 3 points and Player B scores 3 points', () => {
+            const result = scoreToDisplay(3, 3)
+            expect(result).toBe('Deuce')
+        })
+        test('Should display Advantage-40 if Player A scores 4 points and Player B scores 3 points', () => {
+            const result = scoreToDisplay(4, 3)
+            expect(result).toBe('Advantage-40')
+        })
+        test('Should display Love-15 if Player B scores 1 point', () => {
+            const result = scoreToDisplay(0, 1)
+            expect(result).toBe('Love-15')
+        })
+        test('Should display Love-30 if Player B scores 2 points', () => {
+            const result = scoreToDisplay(0, 2)
+            expect(result).toBe('Love-30')
+        })
+        test('Should display Love-40 if Player B scores 3 points', () => {
+            const result = scoreToDisplay(0, 3)
+            expect(result).toBe('Love-40')
+        })
+        test('Should display 15-30 if Player A scores 1 point and Player B scores 2 points', () => {
+            const result = scoreToDisplay(1, 2)
+            expect(result).toBe('15-30')
+        })
+        test('Should display 30-40 if Player A scores 2 points and Player B scores 3 points', () => {
+            const result = scoreToDisplay(2, 3)
+            expect(result).toBe('30-40')
+        })
+        test('Should display 40-Advantage if Player A scores 3 points and Player B scores 4 points', () => {
+            const result = scoreToDisplay(3, 4)
+            expect(result).toBe('40-Advantage')
+        })
     })
 })
